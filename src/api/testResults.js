d24bc6ea4f296170e8e3c30d72dff70502ce3b3c
Juha Yoon <ijooha16@gmail.com>
2025-02-25 16:31:07 +0900

    fix: glitch 주소 변경

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -2,22 +2,26 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_MBTI;
 
+const api = axios.create({
+  baseURL: API_URL,
+});
+
 export const getTestResults = async () => {
-  const response = await axios.get(API_URL);
+  const response = await api.get(API_URL);
   return response.data;
 };
 
 export const createTestResult = async (resultData) => {
-  const response = await axios.post(API_URL, resultData);
+  const response = await api.post(API_URL, resultData);
   return response.data;
 };
 
 export const deleteTestResult = async (id) => {
-  const response = await axios.delete(`${API_URL}/${id}`);
+  const response = await api.delete(`${API_URL}/${id}`);
   return response.data;
 };
 
 export const updateTestResultVisibility = async (id, visibility) => {
-  const response = await axios.patch(`${API_URL}/${id}`, {visibility});
+  const response = await api.patch(`${API_URL}/${id}`, { visibility });
   return response.data;
 };
